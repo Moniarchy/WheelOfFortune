0866ba89bacc83c8345a1adffea1ae441445e0d7
Margarita Tagaban <v-mataga@microsoft.com>
2018-03-27 16:30:00 -0700

    added GuessLetter Class and added PromptForLetter method.

---
+++ WheelOfFortune/WheelOfFortune/GuessLetter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune
+{
+    class GuessLetter
+    {
+        public string PromptForLetter()
+        {
+            Console.Write("Write a letter: ");
+            string letter = Console.ReadLine();
+            return letter;
+        }
+    }
+
+
+}
