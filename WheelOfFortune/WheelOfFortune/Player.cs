b6a696614edd917baccf341228c1fbbe57ad4ba4
Monika Sharma <v-monsha@microsoft.com>
2018-03-27 16:05:39 -0700

    add Game and Player class

---
+++ WheelOfFortune/WheelOfFortune/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune
+{
+    class Player
+    {
+        public string Name;
+        public int TotalMoney { get; set; }
+
+
+    }
+}
