4abc0e9892a798a79e05f4764405afbbb16c366e
Margarita Tagaban <v-mataga@microsoft.com>
2018-03-27 16:49:45 -0700

    updated Game Class with instance data variable word.

---
+++ WheelOfFortune/WheelOfFortune/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune
+{
+    class Game
+    {
+        //TODO Gettere Setter for each fields
+        public int Round { get; set; }
+        public readonly static Char[] Vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+        //TODO add more consonants
+        public readonly static Char[] Consonants = new[] { 'b', 'c', 'd' };
+        public int Turn;
+        public bool IsGameOver;
+        public int WheelValue;
+        public int NoOfPlayer;
+        public Word word = new Word("Microsoft");
+        public Game()
+        {
+
+        }
+    }
+}
