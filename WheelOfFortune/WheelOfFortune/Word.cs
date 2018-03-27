c0496e075349aa7d8dd3a6794db562ea1b43542f
Margarita Tagaban <v-mataga@microsoft.com>
2018-03-27 16:54:21 -0700

    added constructor for Word Class which takes a string answer and initalizes data variable Answer.

---
+++ WheelOfFortune/WheelOfFortune/Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune
+{
+    class Word
+    {
+        public string Answer;
+        public string[] AlreadyGuessedLetters;
+        public string CurrentlyDisplayedString;
+        public Word(string answer)
+        {
+            this.Answer = answer;
+        }
+        public void HideWord()
+        {
+            //Matt
+        }
+        public string ShowGuessedLetters()
+        {
+            //Areanna
+            return "";
+        }
+    }
+}
+
