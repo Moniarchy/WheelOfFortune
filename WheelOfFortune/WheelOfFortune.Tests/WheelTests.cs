c2e11e70da2a406ecbeb24b8613ddbc592fd0605
Monika Sharma <v-monsha@microsoft.com>
2018-03-28 19:55:36 -0700

    Added Wheel source and test class

---
+++ WheelOfFortune/WheelOfFortune.Tests/WheelTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WheelOfFortune.Tests
+{
+    [TestClass]
+    public class WheelTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var wheel = new Wheel();
+            var expected = 900;
+            var actual = wheel.SelectedSpoke;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSpin()
+        {
+            var wheel = new Wheel();
+            var expected = wheel.SelectedSpoke;
+            var actual = wheel.Spin();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WheelOfFortune/WheelOfFortune/Wheel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortune
+{
+    public class Wheel
+    {
+        //0 : Lose A Turn   -9: Bankrupt
+        public static List<int> Spokes = new List<int> { 500, 600, 700, 900, 2500, 0, -9 };
+        public int SelectedSpoke { get; set; }
+
+        public Wheel()
+        {
+            SelectedSpoke = 900;
+        }
+
+       /// <summary>
+       /// Spin returns randomly generate value given in Spokes field
+       /// </summary>
+       /// <returns>SelectedSpoke</returns>
+
+        public int Spin()
+        {
+            //TODO : Calculate random no generator from Wheel's options
+            //Set SelectedSpoke
+            return SelectedSpoke;
+        }
+    }
+}
