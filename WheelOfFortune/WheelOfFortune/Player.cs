eb209a18c4c579c5938e582b1015996f2c61e99a
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-03-29 15:59:08 -0700

    Changed wording slightly in doc to make RoundTotal's purpose more clear

--- WheelOfFortune/WheelOfFortune/Player.cs
+++ WheelOfFortune/WheelOfFortune/Player.cs
@@ -10,7 +10,7 @@ namespace WheelOfFortune
     /// The Player class represents both human players and computer players
     /// It holds a string Name, and two fields representing money: GameTotal and RoundTotal
     /// GameTotal is the sum of all money earned by the player
-    /// RoundTotal is the amount of money earned by the current round of the game
+    /// RoundTotal is the amount of money earned by the player, during the current round of the game
     /// </summary>
     public class Player
     {
@@ -20,12 +20,12 @@ namespace WheelOfFortune
         public string Name { get; private set; }
 
         /// <summary>
-        /// GameTotal: An int representing the sum of money the player has earned across all rounds. It always starts at 0. It can be negative.
+        /// GameTotal: An int representing the sum of money the player has earned across all rounds. It always starts at 0, but increases and decreases across the rounds of the game. It can be negative.
         /// </summary>
         public int GameTotal { get; set; } = 0;
 
         /// <summary>
-        /// RoundTotal: An int representing the money earned by the player during the current round. It gets reset every round to its intial value, 0. It can be negative
+        /// RoundTotal: An int representing the money earned by the player during the current round. It gets reset every round to its intial value, 0. It can be negative.
         /// </summary>
         public int RoundTotal { get; set; } = 0;
 
